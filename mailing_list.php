8c49dce3ff1d2167616f0d90151fbc6d669fa7ba
Christopher Collins <chris@spring215.com>
2015-04-19 10:29:17 -0400

    email push change

--- mailing_list.php
+++ mailing_list.php
@@ -6,7 +6,7 @@ $contact_email = $_POST['contact_email'];
 $contact_month = $_POST['contact_month'];
 $contact_day = $_POST['contact_day'];
 $contact_year = $_POST['contact_year'];
-$to = 'chris@spring215.com';
+$to = 'domelife@gmail.com';
 $subject = 'New Red Rock Email Contact:' . $contact_name_last;
 
 $message = 'FIRSTNAME: ' . $contact_name_first . "\n" . 'LAST NAME: ' . $contact_name_last . "\n" . 'EMAIL: ' . $contact_email . "\n" . 'BIRTHDAY: ' . $contact_month . " " . $contact_day . ',' .  $contact_year;
